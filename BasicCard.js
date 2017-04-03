99593e6541de262289df5b6206530c54824dc3e8
Karns <pkarns@deloitte.com>
2017-04-03 11:46:42 -0400

    BasicCard game now working. Need to add additional questions still

--- BasicCard.js
+++ BasicCard.js
@@ -34,33 +34,38 @@ console.log("New card: " + flashcardArray[i].front + flashcardArray[i].back);
 
 //counts the number of correct answers
 var correct = 0;
-//counts number of questions already asked
-var questionsAsked = 0;
 
 //setting up a recursive function to ask each of the questions in the array
 //using inquirer npm to ask questions
-var askQuestion = function () {
+var askQuestion = function (questionsAsked) {
 	if (questionsAsked < flashcardArray.length) {
 		inquirer.prompt([
 			{
 				type: "input",
-				message: newFlashcard.front, 
+				message: flashcardArray[questionsAsked].front, 
 				name: "answer"		
 			}
 		//confirm whether the user's answer is correct or not
 		]).then(function(answers){
-				console.log(answers);
-				console.log(answers.answer);
-				if (answers.answer === newFlashcard.back) {
+				//console.log(answers);
+				//console.log(answers.answer);
+				if (answers.answer === flashcardArray[questionsAsked].back) {
 					console.log("you are correct");
 					correct++;
 				}//if end
 				else {
-					console.log("Wrong, the correct answer is: " + newFlashcard.back);
+					console.log("Wrong, the correct answer is: " + flashcardArray[questionsAsked].back);
 				}//else end
+
+				//add 1 to questionsAsked
+				questionsAsked++;
+				//ask next question 
+				askQuestion(questionsAsked);
 			});//answers func end
 	}//end of: if (questionsAsked < flashcardArray.length)
 	else {
-
+		console.log("Questions you got correct: " + correct);
+		console.log("game over");
 	}//else end
 };//askQuestion function end
+askQuestion(0);
